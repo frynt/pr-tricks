817a65b45dde29a4eb5a3ffd4d0e485408694c91
hbarbi <hugo.barbi@etu.unistra.fr>
2021-04-07 11:34:48 +0200

    Adding options.js and testing api storage

--- public/options.js
+++ public/options.js
@@ -4,31 +4,29 @@
 // Saves options to chrome.storage
 function save_options() {
 	let color = document.getElementById('color').value;
-	let likesColor = document.getElementById('like').checked;
 	chrome.storage.sync.set({
 		favoriteColor: color,
-		likesColor: likesColor
 	}, function () {
 		// Update status to let user know options were saved.
 		let status = document.getElementById('status');
 		status.textContent = 'Options saved.';
 		setTimeout(function () {
 			status.textContent = '';
-		}, 750);
+		}, 2000);
 	});
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-	// Use default value color = 'red' and likesColor = true.
+	// Use default value color = 'red'
 	chrome.storage.sync.get({
 		favoriteColor: 'red',
-		likesColor: true
 	}, function (items) {
 		document.getElementById('color').value = items.favoriteColor;
-		document.getElementById('like').checked = items.likesColor;
+		document.body.style.backgroundColor = document.getElementById('color').value;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
+
