bbacc26f1f134fdcc4da95a115271e44c5a54ca2
hbarbi <hugo.barbi@etu.unistra.fr>
2021-04-07 15:20:14 +0200

    Adding style to options page

--- public/options.js
+++ public/options.js
@@ -4,8 +4,10 @@
 // Saves options to chrome.storage
 function save_options() {
 	let color = document.getElementById('color').value;
+	let formation = document.getElementById('formation').checked;
 	chrome.storage.sync.set({
 		favoriteColor: color,
+		favoriteFormation: formation,
 	}, function () {
 		// Update status to let user know options were saved.
 		let status = document.getElementById('status');
@@ -19,14 +21,14 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-	// Use default value color = 'red'
 	chrome.storage.sync.get({
-		favoriteColor: 'red',
+		favoriteColor: 'white',
+		favoriteFormation: 'off',
 	}, function (items) {
 		document.getElementById('color').value = items.favoriteColor;
 		document.body.style.backgroundColor = document.getElementById('color').value;
+		document.getElementById('formation').checked = items.favoriteFormation;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
-
