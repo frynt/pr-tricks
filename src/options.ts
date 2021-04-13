2d0d65476b0d97742b4594cdfe14b8c7e29bc9a7
hbarbi <hugo.barbi@etu.unistra.fr>
2021-04-13 12:02:20 +0200

    generated-trickList-categories

--- src/options.ts
+++ src/options.ts
@@ -1,6 +1,7 @@
+import { castArray } from 'lodash';
+import { trickList } from './data/trick-list';
 
 // Doc from https://developer.chrome.com/docs/extensions/mv3/options/
-export { }
 // Saves options to chrome.storage
 export function save_options() {
 	let color = (<HTMLInputElement>document.getElementById('color')).value;
@@ -32,3 +33,36 @@ export function restore_options() {
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
+
+// Get each unique categories from trickList array
+export function get_categories() {
+	let categories = [];
+	trickList.forEach(element => filter(element));
+
+	function filter(element) {
+		if (!categories.includes(element.name)) {
+			categories.push(element.name);
+		}
+	}
+	return categories;
+}
+
+export function display_categories() {
+	let div = document.getElementById('categories');
+	let list = document.createElement('ul');
+	let tab = get_categories();
+
+	tab.forEach(element => display_element(element));
+
+	function display_element(element) {
+		let label = document.createElement('label');
+		let input = document.createElement('input');
+		input.setAttribute("type","checkbox");
+		label.innerHTML = element;
+		list.appendChild(label);
+		list.appendChild(input);
+		list.appendChild(document.createElement('br'));
+	}
+	div.appendChild(list);
+}
+display_categories();
